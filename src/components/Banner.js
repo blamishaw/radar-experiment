b270ef0ba3da4a94ea7a4e7695a6618246f1e721
Brendan Lamishaw <blamishaw@gmail.com>
2022-09-11 12:10:45 -0400

    updated banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,8 +4,8 @@ const Banner = () => {
     return (
         <div className="banner-area">
             <img className="banner-icon" src="/assets/radar.png" />
-            <span className="banner-text">Product Engineer Interview</span>
-            <span className="fa fa-github fa-2x" onClick={() => window.location.href = "https://github.com/blamishaw/radar-interview"}></span>
+            <span className="banner-text">Radar API Test</span>
+            <span className="fa fa-github fa-2x" onClick={() => window.location.href = "https://github.com/blamishaw/radar-experiment"}></span>
         </div>
         
     );
